ff02f3a00c7cca05923938a0a38841c0a76208ce
ashishbg <ashishbg.g@gmail.com>
2023-08-26 21:49:57 +0530

    simple job program

--- src/main/java/com/ashishbg/Main.java
+++ src/main/java/com/ashishbg/Main.java
@@ -1,7 +1,38 @@
 package com.ashishbg;
 
+import org.quartz.JobBuilder;
+import org.quartz.JobDetail;
+import org.quartz.Scheduler;
+import org.quartz.TriggerBuilder;
+import org.quartz.SchedulerException;
+import org.quartz.SimpleScheduleBuilder;
+import org.quartz.Trigger;
+import org.quartz.impl.StdSchedulerFactory;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scheduler scheduler = null;
+        try {
+            scheduler = StdSchedulerFactory.getDefaultScheduler();
+            scheduler.start();
+
+            JobDetail jobDetail = JobBuilder.newJob(SimpleJob.class)
+                    .withIdentity("simpleJob1")
+                    .withDescription("This is a very simple job")
+                    .build();
+
+            Trigger trigger = TriggerBuilder.newTrigger()
+                    .withIdentity("myTrigger", "group1")
+                    .startNow()
+                    .withSchedule(SimpleScheduleBuilder.simpleSchedule()
+                            .withIntervalInSeconds(5)
+                            .repeatForever())
+                    .build();
+
+            scheduler.scheduleJob(jobDetail, trigger);
+
+        } catch (SchedulerException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
